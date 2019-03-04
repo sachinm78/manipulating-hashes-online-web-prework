e1737a6c4dc50e48a000aa18f95dc5d1b8f1a4a3
Sachin Mahajan <sachinm78@gmail.com>
2019-03-04 22:30:31 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -12,11 +12,7 @@ def first_challenge
       favorite_icecream_flavors: ["strawberry", "cookie dough", "mint chip"]
     }
   }
-
-  #your code here
-
-
-  #remember to return your newly altered contacts hash!
+  contacts["Freddy Mercury"][:favorite_icecream_flavors].delete_if == "strawberry"
   contacts
 end
 

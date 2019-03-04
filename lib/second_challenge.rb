3cf39d5da9cf73888434e00a42c9df7963c5ed42
Sachin Mahajan <sachinm78@gmail.com>
2019-03-04 22:38:58 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -5,5 +5,5 @@ def second_challenge
    meat: ["chicken", "steak", "salmon"],
    grains: ["rice", "pasta"]
   }
-  groceries[:dairy][:vegetable][:meat][:grains].value
+  groceries[:dairy][:vegetable][:meat][:grains].values
 end
